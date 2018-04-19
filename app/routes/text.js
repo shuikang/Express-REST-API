3140e15398801925acd3c7c2251def7881dc432e
boat46450 <boat46450@hotmail.com>
2018-04-19 15:05:04 +0700

    add get

--- app/routes/text.js
+++ app/routes/text.js
@@ -5,5 +5,6 @@ const textController = require('../controllers/text')
 const router = express.Router()
 
 router.get('/', textController.getAll)
+router.get('/:id', textController.get)
 
 module.exports = router
