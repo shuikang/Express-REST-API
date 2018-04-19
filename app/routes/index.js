96e69bd592604b8b4a41250eefeffe75ea2bb589
boat46450 <boat46450@hotmail.com>
2018-04-19 15:04:42 +0700

    change / to string

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json({
-        date: new Date()
-    })
+    res.send(new Date())
 })
 
 router.use('/text', require('./text'))
